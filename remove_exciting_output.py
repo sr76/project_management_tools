798ce034ee1be315d379e2c330b76093e938fd76
sr76 <santiago.rigamonti@physik.hu-berlin.de>
2012-09-04 17:25:00 +0200

    Added remove PLOT.* to remove_exciting_output.py script

--- remove_exciting_output.py
+++ remove_exciting_output.py
@@ -2,6 +2,9 @@
 import os
 
 os.system("rm -f *.OUT*")
+os.system("rm -f PLOT.*")
+os.system("rm -f .INFO*")
+os.system("rm -f info*")
 os.system("rm -f dos.xml")
 os.system("rm -f info.xml")
 os.system("rm -f *.e*")
